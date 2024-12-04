c76bd64eb481abe3f8806c6325a7cd37d3de2d78
subhashinisai <subhashinisaipriyatripathi@gmail.com>
2024-12-04 19:05:11 +0530

    franework done

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const container=document.querySelector('.container');
-const question=document.querySelector('.question');
-const choices=document.querySelector('.choices');
+const questionBox=document.querySelector('.question');
+const choicesBox=document.querySelector('.choices');
 const nextBtn=document.querySelector('.nextBtn');
 
+
+
+
+//array of objects with question and choices with correct answer
 const quiz=[
     {
         question: "Q. Which of the following is not a CSS box model property?",
@@ -24,6 +28,37 @@ const quiz=[
         choices: ["It refers to the current function.", "It refers to the current object.", "It refers to the parent object.", " It is used for comments."],
         answer: "It refers to the current object."
     }
-]
+];
+
+let currentQuestionIndex=0;
+
+//arrow function to show questions
+const showQuestions=()=>{
+const questionDetails = quiz[currentQuestionIndex];
+questionBox.textContent=questionDetails.question;
+
+choicesBox.textContent="";
+for(let i=0;i<questionDetails.choices.length;i++){
+    const currentChoice=questionDetails.choices[i];
+    const choiceDiv=document.createElement('div');
+    choiceDiv.textContent=currentChoice;
+    choiceDiv.classList.add('choice');
+    choicesBox.appendChild(choiceDiv);
+
+}
+// console.log(questionDetails);
+}
+
+
+
+
+showQuestions();
+nextBtn.addEventListener('click',()=>{
+    if(currentQuestionIndex<quiz.length){
+        currentQuestionIndex++;
+        showQuestions();
+    }
+    
+});
 
 
